a796417f9742b73ee7a75e23b2a9e28a6432c0fb
Jonathan-0708 <jonathandc0708@gmail.com>
2021-08-06 14:46:29 +0000

    modelo final

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, VARCHAR
+from sqlalchemy import Column, ForeignKey, Integer, String, VARCHAR,Enum,Date,DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -15,6 +15,7 @@ class User(Base):
     name = Column(String(250), nullable=False)
     last_name = Column(String(250), nullable=False)
     email = Column(VARCHAR(50), nullable=False,primary_key=True,unique=True)
+    date_of_birth = Column(Date,nullable=False)
 
 class Follower(Base):
     __tablename__ = 'follower'
@@ -28,6 +29,7 @@ class Post(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+    publication_date = Column(DateTime, nullable=True)
 
 class Comment(Base):
     __tablename__ = 'comment'
@@ -37,9 +39,44 @@ class Comment(Base):
     user = relationship(User)
     post_id = Column(Integer,ForeignKey('post.id'))
     post = relationship(Post)
-    
+    follower_id = Column(Integer, ForeignKey('follower.id'))
+    follower = relationship(Follower)
 
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum,nullable=False)
+    ulr = Column(String(250), nullable=False, unique=True)
+    post_id = Column(Integer,ForeignKey('post.id'))
+    post = relationship(Post)
 
+class Stories(Base):
+    __tablename__ = 'stories'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer,ForeignKey('post.id'))
+    post = relationship(Post)
+    media_id = Column(Integer, ForeignKey('media.id'))
+    media = relationship(Media)   
+
+class Reels(Base):
+    __tablename__ = 'reels'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer,ForeignKey('post.id'))
+    post = relationship(Post)
+    media_id = Column(Integer, ForeignKey('media.id'))
+    media = relationship(Media)
+    stories_id = Column(Integer,ForeignKey('stories.id'))
+    stories = relationship(Stories)  
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    follower_id = Column(Integer, ForeignKey('follower.id'))
+    follower = relationship(Follower)
+    stories_id = Column(Integer,ForeignKey('stories.id'))
+    stories = relationship(Stories) 
+    reels_id =Column(Integer,ForeignKey('reels.id'))
+    reels = relationship(Reels)
 
 ## Draw from SQLAlchemy base
 try:
